803350099319faef2ccd6db8b4319884d165c1b0
fuadhasan08 <fuadhasandipro08@gmail.com>
2026-03-25 13:42:57 +0600

    js done

--- Lab Task 05 - Text Analyzer/JS/script.js
+++ Lab Task 05 - Text Analyzer/JS/script.js
@@ -1,1 +1,23 @@
 console.log("Connected");
+
+function analyze_data() {
+  let text = document.getElementById("InputText").value;
+
+  let wordsArray = text.split(" ");
+  let wCount = 0;
+
+  for (let i = 0; i < wordsArray.length; i++) {
+    if (wordsArray[i].trim() !== "") {
+      wCount++;
+    }
+  }
+
+  let cCount = text.length;
+  let rText = text.split("").reverse().join("");
+
+  document.getElementById("charCount").innerHTML = cCount;
+  document.getElementById("wordCount").innerHTML = wCount;
+  document.getElementById("reversedText").innerHTML = rText;
+
+  return false;
+}
